866412764696cb28e8857f9eae1c99453dbda0e3
Arantxa Ordoyo <44567127+arianxa@users.noreply.github.com>
2025-11-20 16:53:09 +0000

    generador

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function () {
+   const pronoun = ['the', 'our'];
+  const adj = ['great', 'big'];
+  const noun = ['jogger', 'racoon'];
+  const ext=['.com','.net','.us','.io','.etc'];
+
+  for (let i =0;i<pronoun.length;i++){
+    for(let j=0;j<adj.length;j++){
+      for(let k=0;k<noun.length;k++){
+        for(let h=0;h<ext.length;h++){
+          //Añadimos los array dentro del p y creamos un span dentro de p para darle estilo 
+           document.getElementById("dominio").innerHTML+=`<span class="spanDominio m-1">`+pronoun[i]+adj[j]+noun[k]+ext[h]+`</span>`;
+
+
+     
+        }
+      }
+    }
+  }
+ };
+
+//
